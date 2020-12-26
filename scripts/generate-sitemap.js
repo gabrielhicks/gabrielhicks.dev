1492a9d1ddde32524f82cc10fdf46b5dc1b4322d
gabrielhicks <gabrielhicks@mail.com>
2020-12-26 14:41:24 -0500

    Fixing sitemap

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -3,9 +3,16 @@ const fs = require('fs');
 const globby = require('globby');
 const prettier = require('prettier');
 
+const getDate = new Date().toISOString();
+
 (async () => {
   const prettierConfig = await prettier.resolveConfig('./.prettierrc.js');
-  const pages = await globby(['pages/**/*.js', '!pages/_*.js', '!pages/api']);
+  const pages = await globby([
+    'pages/**/*.js',
+    'pages/*.js',
+    '!pages/api',
+    '!pages/_*.js'
+  ]);
   const sitemap = `
         <?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -17,6 +24,7 @@ const prettier = require('prettier');
                 return `
                         <url>
                             <loc>${`https://gabrielhicks.dev${route}`}</loc>
+                            <lastmod>${getDate}</lastmod>
                         </url>
                     `;
               })
@@ -24,11 +32,22 @@ const prettier = require('prettier');
         </urlset>
     `;
 
-  const formatted = prettier.format(sitemap, {
+  const generatedSitemap = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+    >
+      ${sitemap}
+    </urlset>
+  `;
+
+  const formatted = prettier.format(generatedSitemap, {
     ...prettierConfig,
     parser: 'html'
   });
 
   // eslint-disable-next-line no-sync
-  fs.writeFileSync('public/sitemap.xml', formatted);
+  fs.writeFileSync('./public/sitemap.xml', formatted, 'utf8');
 })();
