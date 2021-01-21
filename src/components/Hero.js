138babf638b6b02cbecfd5553c4410fc871fbd0e
gabrielhicks <gabrielhicks@mail.com>
2021-01-21 10:29:32 -0500

    center text change size

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { Flex, Heading, Grid } from '@chakra-ui/react';
 
 const Hero = ({ title, subtitle }) => (
-  <Flex justifyContent="center" alignItems="center" height="80vh">
+  <Flex
+    textAlign="center"
+    justifyContent="center"
+    alignItems="center"
+    height="80vh"
+  >
     <Grid templateRows="repeat(2, 1fr)" gap={2}>
-      <Heading as="h1" fontSize="8vw">
+      <Heading as="h1" fontSize="11vw">
         {title}
       </Heading>
-      <Heading fontWeight="normal" fontSize="4vw">
+      <Heading fontWeight="normal" fontSize="5.5vw">
         {subtitle}
       </Heading>
     </Grid>
