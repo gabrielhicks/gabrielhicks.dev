d21c8645d41b394b3e3ea2d09a64091b99be6836
gabrielhicks <gabrielhicks@mail.com>
2021-02-03 12:22:42 -0500

    Updating analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,8 @@
 /* eslint-disable eslint-comments/disable-enable-pair */
-/* eslint-disable react/no-danger */
 
 import React from 'react';
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 import { ColorModeScript } from '@chakra-ui/react';
-
-import { GA_TRACKING_ID } from '../lib/gtag';
 
 export default class Document extends NextDocument {
   render() {
@@ -14,19 +11,7 @@ export default class Document extends NextDocument {
         <Head>
           <script
             async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `
-            }}
+            src={`https://www.googletagmanager.com/gtag/js?id=G-2FHE2V2W9T`}
           />
           <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
           <meta name="msapplication-TileColor" content="#ffc40d" />
